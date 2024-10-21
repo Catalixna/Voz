75581181f6cf894ccde6dbc87a50282ba33ef642
Catalixna <ccardenasaa@gmail.com>
2024-10-21 10:31:02 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -15,7 +15,7 @@ from gtts import gTTS
 from googletrans import Translator
 
 
-st.title("TÚ chinito de confianza")
+st.markdown("<h1 style='text-align: center; color: #3c7136;'>TÚ chinito de confianza</h1>", unsafe_allow_html=True)
 st.subheader("¡¡Te ayudare a traducir!!")
 
 
